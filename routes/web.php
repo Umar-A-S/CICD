4d4453bca0a5c7b78f6d1fbad61c5d8fbe5b6939
Umar-A-S <umaralfisyahran@gmail.com>
2025-12-31 01:58:26 +0000

    test error CI

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
-}
+});
 
 Route::get('about', function () {
     return view('about', ['title' => 'About']);
