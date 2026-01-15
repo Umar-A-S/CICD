ba6d4dea0838e1338b0e380d279c840582f9ee36
Umar-A-S <umaralfisyahran@gmail.com>
2026-01-15 10:09:02 +0700

    remove all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Selaksa
-CI test
--- resources/views/components/header.blade.php
+++
@@ -1,5 +0,0 @@
-<header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6">
-        <h1 class="text-3xl font-bold text-gray-900">{{ $slot }}</h1>
-    </div>
-</header>
