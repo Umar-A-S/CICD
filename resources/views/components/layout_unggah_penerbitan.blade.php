9d0de50cb7108161262e9f6c21f7e6bf2cfab1c2
Umar-A-S <umaralfisyahran@gmail.com>
2026-02-02 22:01:11 +0700

    Merge main with branch superadmin v3.1.3

--- resources/views/components/layout_unggah_penerbitan.blade.php
+++ resources/views/components/layout_unggah_penerbitan.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style_kakot.css">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
